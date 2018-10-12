7bf23857c6e3d402aafa0e867a770252e1016d9c
lixanfajardo <lixan@bywave.com.au>
2018-10-12 17:21:02 +0800

    added exercise 2 and 3

---
+++ assignment2.py
@@ -0,0 +1,8 @@
+_name_ = raw_input("Enter email: ")
+_author_ = "lixan@bywave.com.au"
+
+if _name_ == _author_:
+	for number in range(0, 100, 7):
+		print number
+else:
+	print "Invalid email. Email is", _author_
